d1677a3102c791fd029541e63a55213f6ac16a1d
Denis Shagimuratov <poluxster@gmail.com>
2014-04-14 12:00:57 +0600

    Fixed mcp_queue.php bug.

--- root/install/index.php
+++ root/install/index.php
@@ -117,6 +117,12 @@ $versions = array(
 				 'auth'
 		    ),
     ),
+	'3.75' => array(
+	   	// no changes ... purge caches anyways
+		 'cache_purge' => array(
+				 'auth'
+		    ),
+    ),
 );
 
 // Include the UMIL Auto file and everything else will be handled automatically.
